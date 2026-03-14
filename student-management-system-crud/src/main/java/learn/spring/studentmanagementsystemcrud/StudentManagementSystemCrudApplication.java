19d28eadc90c3c56f05556b7a392c2e9bb83ab83
gnaveenkumar91 <naveeng2k04@gmail.com>
2026-03-14 13:38:44 +0530

    Student management system with thymleaff

---
+++ student-management-system-crud/src/main/java/learn/spring/studentmanagementsystemcrud/StudentManagementSystemCrudApplication.java
@@ -0,0 +1,14 @@
+package learn.spring.studentmanagementsystemcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemCrudApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemCrudApplication.class, args);
+	}
+
+}
